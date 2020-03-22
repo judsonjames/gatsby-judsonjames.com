b6c5b2123b5058eb4968d4c8713b62f2b6676395
judsonjames <judsonjames96@gmail.com>
2020-03-22 02:54:45 -0400

    test fixes

--- config/website.js
+++ config/website.js
@@ -3,9 +3,6 @@
  * Website and SEO configurations
  * @NOTE before going in too deep reading this, please refer to the following files:
  *   - https://github.com/DannyHinshaw/gatsby-nulleffort/blob/master/src/components/SEO.tsx
- *
- * This file is meant to mitigate both implementations into a unified SEO from the original 52inc site to
- * have the fastest implementation possible, as well as the most readable solution.
  */
 
 module.exports = {
