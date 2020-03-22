fdc0c95bf3adc9ccb85d6eac40fb29a21df69d6f
judsonjames <judsonjames96@gmail.com>
2020-03-22 02:35:37 -0400

    test fixes

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -5,6 +5,7 @@ import config from "../../config/website";
 export interface ISEOProps {
     title: string;
     description: string;
+    currentSite: string;
 }
 
 /**
@@ -12,11 +13,12 @@ export interface ISEOProps {
  * @constructor
  */
 const SEO = (props: ISEOProps): JSX.Element => {
-    const {title, description} = props;
+    const {title, description, currentSite} = props;
     const realPrefix: string = config.pathPrefix === "/" ? "" : config.pathPrefix;
     const image = config.siteUrl + realPrefix + config.siteLogo;
     return (
         <Helmet>
+            {/* Base HTML Meta Tags*/}
             <html lang={config.siteLanguage}/>
             <title>{title}</title>
             <meta name={"description"} content={description}/>
@@ -24,6 +26,19 @@ const SEO = (props: ISEOProps): JSX.Element => {
             <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1"/>
             <meta name="msapplication-tap-highlight" content="no"/>
             <link rel={"canonical"} href={config.siteUrl}/>
+
+            {/* Google */}
+            <meta itemProp={"name"} content={currentSite}/>
+            <meta itemProp={"description"} content={description}/>
+            <meta itemProp={"image"} content={image}/>
+
+            {/* Facebook */}
+            <meta property="og:url" content="https://focused-lamarr-93f88f.netlify.com/"/>
+            <meta property="og:type" content="website"/>
+            <meta property="og:title" content={title}/>
+            <meta property="og:description" content={description}/>
+            <meta property="og:image" content={image}/>
+
             {/* Twitter */}
             <meta name="twitter:card" content="summary_large_image"/>
             <meta name="twitter:creator" content={config.userTwitter ? config.userTwitter : ""}/>
