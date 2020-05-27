001f03de28503bab69784fd04ae7fbda5c2d2479
judsonjames <judsonjames96@gmail.com>
2020-05-26 23:39:13 -0400

    FORM :: Removing GetForm usage in exchange to use Netlify's native forms

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,4 +1,6 @@
+import {ErrorMessage, Field, Form, Formik} from "formik";
 import React, {FormEvent, useState} from "react";
+import * as Yup from "yup";
 import SiteLayout from "../../components/SiteLayout";
 
 const Contact = (): JSX.Element => {
@@ -10,18 +12,22 @@ const Contact = (): JSX.Element => {
     const handleSetEmail = (e: FormEvent<HTMLInputElement>) => setEmail(e.currentTarget.value);
     const handleSetMessage = (e: FormEvent<HTMLInputElement>) => setMessage(e.currentTarget.value);
 
-    const submit = () => {
-        const url: string = "https://getform.io/f/afbddb2e-ea78-48ce-b785-7fd6f834f64d";
-        fetch(url, {
-            body: JSON.stringify({ email, message, name }),
-            headers: {
-                "Accept": "application/json",
-                "Content-Type": "json",
-            },
-            method: "POST",
-        })
-            .then(() => alert("success!"))
-            .catch(() => alert("fail!"));
+    const contactFormSchema = Yup.object().shape({
+        email: Yup.string()
+            .email("Invalid email")
+            .required("Required"),
+        message: Yup.string()
+            .min(2, "Message Min Length: 2")
+            .max(250, "Message Max Length: 250")
+            .required("Required"),
+        name: Yup.string()
+            .min(2, "Name Min Length: 2")
+            .max(50, "Name Max length: 50")
+            .required("Required"),
+    });
+
+    const contactFormSubmit = () => {
+        alert("Success!");
     };
 
     return (
@@ -30,12 +36,35 @@ const Contact = (): JSX.Element => {
             description={"Contact Judson for more information."}
             imageRef={"/img/profile_pic.jpg"}
         >
-            <form>
-                <input type={"text"} name={"name"} onInput={handleSetName}/>
-                <input type={"email"} name={"email"} onInput={handleSetEmail}/>
-                <input type={"text"} name={"message"} onInput={handleSetMessage}/>
-                <button onClick={submit}>Send</button>
-            </form>
+            <div>
+                <Formik
+                    initialValues={{
+                        "bot-field": "",
+                        "form-name": "contact-form",
+                        email, message, name,
+                    }}
+                    onSubmit={contactFormSubmit}
+                    validationSchema={contactFormSchema}
+                >
+                    <Form
+                        data-netlify={"true"}
+                        data-netlify-honeypot={"bot-field"}
+                        data-netlify-recaptcha={"true"}
+                        name={"contact-form"}
+                        noValidate={true}
+                    >
+                        <Field type={"hidden"} name={"bot-field"}/>
+                        <Field type={"hidden"} name={"form-name"}/>
+                        <Field type={"text"} name={"name"}/>
+                        <ErrorMessage name={"name"}/>
+                        <Field type={"email"} name={"email"}/>
+                        <ErrorMessage name={"email"}/>
+                        <Field type={"text"} name={"message"}/>
+                        <ErrorMessage name={"message"}/>
+                        <button type={"submit"}>Submit</button>
+                    </Form>
+                </Formik>
+            </div>
         </SiteLayout>
     );
 };
