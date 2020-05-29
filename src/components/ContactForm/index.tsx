9c1a69553c45e9e4c733c605982a86b59697983e
judsonjames <judsonjames96@gmail.com>
2020-05-28 21:54:31 -0400

    Cleaning up Form Data

--- src/components/ContactForm/index.tsx
+++ src/components/ContactForm/index.tsx
@@ -56,33 +56,23 @@ const ContactForm = (): JSX.Element => {
         validationSchema={contactFormSchema}
       >
         {({errors, touched}) => (
-          <Form
-            data-netlify={"true"}
-            data-netlify-honeypot={"bot-field"}
-            data-netlify-recaptcha={"true"}
-            name={"contact-form"}
-            noValidate={true}
-          >
-            <Field type={"hidden"} name={"bot-field"}/>
-            <Field type={"hidden"} name={"form-name"}/>
-            <div id={"ContactFields"}>
-              <label className={errors.name && touched.name ? "form__error" : undefined}>
-                Your name {errors.name && touched.name ? ` | ${errors.name}` : ""}
-              </label>
-              <Field type={"text"} name={"name"} className={"form__input"}/>
+          <div id={"ContactFields"}>
+            <label className={errors.name && touched.name ? "form__error" : undefined}>
+              Your name {errors.name && touched.name ? ` | ${errors.name}` : ""}
+            </label>
+            <Field type={"text"} name={"name"} className={"form__input"}/>
 
-              <label className={errors.email && touched.email ? "form__error" : undefined}>
-                Your Email {errors.email && touched.email ? ` | ${errors.email}` : ""}
-              </label>
-              <Field type={"email"} name={"email"} className={"form__input"}/>
+            <label className={errors.email && touched.email ? "form__error" : undefined}>
+              Your Email {errors.email && touched.email ? ` | ${errors.email}` : ""}
+            </label>
+            <Field type={"email"} name={"email"} className={"form__input"}/>
 
-              <label className={errors.message && touched.message ? "form__error" : undefined}>
-                A Friendly Message {errors.message && touched.message ? ` | ${errors.message}` : ""}
-              </label>
-              <Field as={"textarea"} name={"message"} className={"form__input message"}/>
-              <button type={"submit"}>Submit</button>
-            </div>
-          </Form>
+            <label className={errors.message && touched.message ? "form__error" : undefined}>
+              A Friendly Message {errors.message && touched.message ? ` | ${errors.message}` : ""}
+            </label>
+            <Field as={"textarea"} name={"message"} className={"form__input message"}/>
+            <button type={"submit"}>Submit</button>
+          </div>
         )}
       </Formik>
     </div>
