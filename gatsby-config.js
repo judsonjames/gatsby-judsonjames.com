ee82a6153f027207b9d1da08335e17e6decd4536
judsonjames <judsonjames96@gmail.com>
2020-05-31 22:57:55 -0400

    Checking for PWA Support

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,15 +22,27 @@ module.exports = {
     `gatsby-plugin-sass`,
     `gatsby-plugin-typescript`,
     `gatsby-plugin-tslint`,
-    `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sitemap`,
     `gatsby-plugin-react-svg`,
     `gatsby-plugin-smoothscroll`,
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Judson James Professional Portfolio`,
+        short_name: `Judson James`,
+        start_url: `/`,
+        background_color: `#4b90d2`,
+        theme_color: `#d18c4b`,
+        display: `standalone`,
+        icon: "/static/favicon.ico",
+      },
+    },
+    `gatsby-plugin-offline`,
+    {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS
+        trackingId: process.env.GOOGLE_ANALYTICS,
       }
     },
     {
